6df3b2b381ecca015810a20e04eea85303b67b0d
Suryanarayan Panda <116947409+pandasuryanarayan@users.noreply.github.com>
2024-05-20 20:04:50 +0530

    Update Task.js

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String },
-  board: { type: mongoose.Schema.Types.ObjectId, ref: 'Board', required: true },
-  completed: { type: Boolean, default: false },
+  name: { type: String, required: true },
+  boardId: { type: mongoose.Schema.Types.ObjectId, ref: 'Board', required: true },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const Task = mongoose.model('Task', taskSchema);
