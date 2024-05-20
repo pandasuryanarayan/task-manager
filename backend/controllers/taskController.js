35b160d2fc36951298032c21827d489bdf0e729c
Suryanarayan Panda <116947409+pandasuryanarayan@users.noreply.github.com>
2024-05-20 20:05:21 +0530

    Update taskController.js

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,19 +1,23 @@
 const Task = require('../models/Task');
 
 exports.createTask = async (req, res) => {
-  const { title, description, board } = req.body;
   try {
-    const task = new Task({ title, description, board });
+    const { name, boardId } = req.body;
+    if (!name || !boardId) {
+      return res.status(400).json({ msg: 'Please provide all required fields' });
+    }
+    const task = new Task({ name, boardId });
     await task.save();
     res.status(201).json(task);
   } catch (error) {
-    res.status(400).json({ error: 'Error creating task' });
+    res.status(500).json({ error: 'Server Error' });
   }
 };
 
 exports.getTasks = async (req, res) => {
   try {
-    const tasks = await Task.find({ board: req.params.boardId });
+    const tasks = await Task.find({ boardId: req.params.boardId });
+    console.log(tasks);
     res.json(tasks);
   } catch (error) {
     res.status(400).json({ error: 'Error fetching tasks' });
