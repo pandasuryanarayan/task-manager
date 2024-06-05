2c0038f17b5a766fa03c5d0113df99b720a83b7d
Suryanarayan Panda <116947409+pandasuryanarayan@users.noreply.github.com>
2024-06-05 21:42:16 +0530

    Update Board.js
    Update Done

--- backend/models/Board.js
+++ backend/models/Board.js
@@ -1,9 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const boardSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-const Board = mongoose.model('Board', boardSchema);
+const Board = mongoose.model("Board", boardSchema);
 module.exports = Board;
