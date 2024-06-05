bfa53ba6e5136abdf9429b578f8d83614a015ead
Suryanarayan Panda <116947409+pandasuryanarayan@users.noreply.github.com>
2024-06-05 21:43:52 +0530

    Update User.js
    Update Done

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,6 +5,10 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 userSchema.pre('save', async function(next) {
